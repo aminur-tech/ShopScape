0760daa8bc12be5ac129ae8a84b101088ad21b80
aminurrahman4078 <aminurrahman9793@gmail.com>
2026-09-06 08:26:59 +0600

    try to fix

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,26 +1,32 @@
-// components/MetaPixel.tsx
-'use client';
+"use client";
 
-import Script from 'next/script';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 declare global {
   interface Window {
-    fbq: any;
+    fbq: (...args: any[]) => void;
   }
 }
+
+const PIXEL_ID = process.env.NEXT_PUBLIC_FB_PIXEL_ID;
 
 export default function MetaPixel() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  // Fire PageView on every route change (Next.js is an SPA, no full reloads)
+  // PageView on Next.js route changes
   useEffect(() => {
-    if (window.fbq) {
-      window.fbq('track', 'PageView');
-    }
+    if (!window.fbq) return;
+
+    window.fbq("track", "PageView");
   }, [pathname, searchParams]);
+
+  if (!PIXEL_ID) {
+    console.error("Meta Pixel ID is missing");
+    return null;
+  }
 
   return (
     <>
@@ -29,21 +35,30 @@ export default function MetaPixel() {
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          if(!f._fbq)f._fbq=n;
+          n.push=n;
+          n.loaded=!0;
+          n.version='2.0';
+          n.queue=[];
+          t=b.createElement(e);
+          t.async=!0;
+          t.src=v;
+          s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}
+          (window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${process.env.NEXT_PUBLIC_FB_PIXEL_ID}');
+
+          fbq('init', '${PIXEL_ID}');
           fbq('track', 'PageView');
         `}
       </Script>
+
       <noscript>
         <img
           height="1"
           width="1"
-          style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
           alt=""
         />
       </noscript>
